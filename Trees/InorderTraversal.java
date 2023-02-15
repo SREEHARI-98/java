e0522960e6f2df6519cb2a9546e65db173fe8878
pshr <86615584+shr-p@users.noreply.github.com>
2023-02-15 07:28:36 +0000

    InorderTraversal

---
+++ Trees/InorderTraversal.java
@@ -0,0 +1,20 @@
+package Trees;
+import java.util.*;
+public class InorderTraversal {
+    public static void main(String[] args) {
+        TreeNode temp = new TreeNode(1);
+        Stack<TreeNode> stack  = new Stack<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        while(temp != null || !stack.empty()){
+            if(temp != null){
+                stack.push(temp);
+                temp = temp.left;
+            }else{
+                temp = stack.pop();
+                ans.add(temp.val);
+                temp = temp.right;
+            }
+        }
+        System.out.println(ans);
+    } 
+}
